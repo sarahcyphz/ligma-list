73659b1016f05a6d451e34df3f3bb8d43b578d92
sarahcyphz <sarah.cyphers@neudesic.com>
2023-12-20 15:57:09 -0500

    working put request

--- ligma-list/src/pages/Spices/spicePage.tsx
+++ ligma-list/src/pages/Spices/spicePage.tsx
@@ -6,7 +6,9 @@ import axios from "axios";
 import AddSpice from "./addSpice";
 
 type Spice = {
+  id: number;
   name: string;
+  isNeeded: number;
 };
 
 const emptySpices: Spice[] = [];
@@ -15,6 +17,19 @@ const SpicesPage = () => {
   const [spices, setSpices]: [Spice[], (spices: Spice[]) => void] = useState(
     emptySpices
   );
+
+  const handleButtonClick = async (index: number) => {
+    const updatedSpices = [...spices];
+    updatedSpices[index].isNeeded = updatedSpices[index].isNeeded === 0 ? 1 : 0;
+    setSpices(updatedSpices);
+
+    try {
+      await axios.put(`http://localhost:5086/catalog/${updatedSpices[index].id}`, updatedSpices[index]);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
   useEffect(() => {
     axios.get<Spice[]>('http://localhost:5086/catalog',
@@ -41,9 +56,16 @@ const SpicesPage = () => {
       </div>
       <div className="spices-container">
         {spices.map((spice, index) => (
-          <button className="spice-card" id={'spice-${index}'} name={spice.name} value={spice.name}>
+          <button 
+            className={`spice-card ${spice.isNeeded === 1 ? 'spice-card-need' : ''}`} 
+            id={'spice-${index}'} 
+            name={spice.name} 
+            value={spice.name}
+            onClick={() => handleButtonClick(index)}
+          >
           <div className="spice-button" key={index}>
             <h2 className="spice-name">{spice.name}</h2>
+            <h2 className="spice-needed">{spice.isNeeded}</h2>
             <img src={trash} alt="trash" />
           </div>
           </button>
